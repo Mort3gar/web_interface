27c4946081498590f59248391d2cbc88eb74e1de
Mort3gar <2021-01422@students.ssau.ru>
2023-12-12 15:02:04 +0400

    Снова отредачен init

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from config import DB_NAME, DB_PASSWORD
 from app.mysqlConnector import DataBaseHandler
 from app.api.brands import brands_api
 from app.api.clients import clients_api
+from app.api.execution_of_orders import execution_api
 from app.api.orders import orders_api
+from app.api.performers import performers_api
 from app.api.posts import posts_api
 from app.api.product import product_api
 from app.api.staff import staff_api
@@ -17,7 +19,9 @@ dbHandler.loadDump()
 
 app.register_blueprint(brands_api)
 app.register_blueprint(clients_api)
+app.register_blueprint(execution_api)
 app.register_blueprint(orders_api)
+app.register_blueprint(performers_api)
 app.register_blueprint(posts_api)
 app.register_blueprint(product_api)
 app.register_blueprint(staff_api)
