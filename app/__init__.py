ee5677bb701739101e379b2b8c8e97bc838b86d4
Mort3gar <2021-01422@students.ssau.ru>
2023-12-12 11:23:10 +0400

    Изменения в init

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from config import DB_NAME, DB_PASSWORD
 from app.mysqlConnector import DataBaseHandler
 from app.api.brands import brands_api
 from app.api.clients import clients_api
+from app.api.orders import orders_api
+from app.api.posts import posts_api
+from app.api.product import product_api
+from app.api.staff import staff_api
+from app.api.types_of_repairs import types_of_repairs_api
 
 app = Flask(__name__)
 
