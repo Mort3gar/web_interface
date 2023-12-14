f0190669c49b90e0f02a88435cdef10e97cb2b6b
Golosoman <constan.lapin2015@mail.ru>
2023-12-14 14:52:55 +0400

    some sosi bebru

--- app/api/orders.py
+++ app/api/orders.py
@@ -14,7 +14,7 @@ def get_order():
     if 'id' in data:
         if int(data['id']) in unzipOneItem(dbHandler.execute(f"select id from orders")):
             res = dbHandler.execute(
-                f"select orders.id, clients.name, product.name, brand_name, model, warranty_period, order_receipt_date from orders left join clients on clients_id = clients.id left join product on product_id = product.id left join brands on brands_id = brands.id where orders.id = {data['id']}")[
+                f"select orders.id, clients.name, product.name, brand_name, model, warranty_period, order_receipt_date, product.id from orders left join clients on clients_id = clients.id left join product on product_id = product.id left join brands on brands_id = brands.id where orders.id = {data['id']}")[
                 0]
             return json.dumps({
                 "id": res[0],
@@ -23,14 +23,15 @@ def get_order():
                 "brand": res[3],
                 'model': res[4],
                 'warranty_period': str(res[5]),
-                'order_receipt_date': str(res[6])
+                'order_receipt_date': str(res[6]),
+                'product_id': res[7]
             }), 200, {'Content-Type': 'application/json'}
         else:
             return abort(409, OrdersAPIErrors.idErr)
     else:
         res = []
         for item in dbHandler.execute(
-                "select orders.id, clients.name, product.name, brand_name, model, warranty_period, order_receipt_date from orders left join clients on clients_id = clients.id left join product on product_id = product.id left join brands on brands_id = brands.id"):
+                "select orders.id, clients.name, product.name, brand_name, model, warranty_period, order_receipt_date, product.id from orders left join clients on clients_id = clients.id left join product on product_id = product.id left join brands on brands_id = brands.id"):
             res.append({
                 "id": item[0],
                 "client_name": item[1],
@@ -38,7 +39,8 @@ def get_order():
                 "brand": item[3],
                 'model': item[4],
                 'warranty_period': str(item[5]),
-                'order_receipt_date': str(item[6])
+                'order_receipt_date': str(item[6]),
+                "product_id": res[7]
             })
         return json.dumps(res), 200, {'Content-Type': 'application/json'}
 
